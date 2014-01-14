47388547df559042cd0163c24c4a00f9d019f2c4
Jeroen <jeroenooms@gmail.com>
2014-01-13 18:07:31 -0800

    no animation on mobile

--- user.js
+++ user.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
 		$(".usermenu li.active a i").addClass("icon-white")
 	});
 	
-    $('.usermenu li a').on('click', function(e) {
-        e.preventDefault();
-        target = this.hash;
-        $.scrollTo(target, 500);
-   });	
-    
-    
-    //see https://github.com/twitter/bootstrap/issues/6350
-    $('[data-clampedwidth]').each(function () {
-        var elem = $(this);
-        var parentPanel = elem.data('clampedwidth');
-        var resizeFn = function () {
-            var sideBarNavWidth = $(parentPanel).width() - parseInt(elem.css('paddingLeft')) - parseInt(elem.css('paddingRight')) - parseInt(elem.css('marginLeft')) - parseInt(elem.css('marginRight')) - parseInt(elem.css('borderLeftWidth')) - parseInt(elem.css('borderRightWidth'));
-            elem.css('width', sideBarNavWidth);
-        };
+	//scrollTo animation, but not on mobile
+	if(!(/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent)){
+		$('.usermenu li a').on('click', function(e) {
+			e.preventDefault();
+			target = this.hash;
+			$.scrollTo(target, 500);
+		});
+	}
+	
+	//see https://github.com/twitter/bootstrap/issues/6350
+	$('[data-clampedwidth]').each(function () {
+		var elem = $(this);
+		var parentPanel = elem.data('clampedwidth');
+		var resizeFn = function () {
+			var sideBarNavWidth = $(parentPanel).width() - parseInt(elem.css('paddingLeft')) - parseInt(elem.css('paddingRight')) - parseInt(elem.css('marginLeft')) - parseInt(elem.css('marginRight')) - parseInt(elem.css('borderLeftWidth')) - parseInt(elem.css('borderRightWidth'));
+			elem.css('width', sideBarNavWidth);
+		};
 
-        resizeFn();
-        $(window).resize(resizeFn);
-    });    
-	
-	
+		resizeFn();
+		$(window).resize(resizeFn);
+	});	
 });
